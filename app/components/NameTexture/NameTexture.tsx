9ac4dd9f2f543d4d7ef99d7f0ce18bd76c9934d1
Adam Rowe <admrwe@Adams-MBP.gpcom.net>
2024-05-16 21:57:09 -0600

    Mo work

---
+++ app/components/NameTexture/NameTexture.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import styles from "./NameTexture.module.css";
+
+interface LetterProps {
+  letter: string;
+}
+
+const Letter = (props: LetterProps) => {
+  const { letter } = props;
+  /**
+   * TODO: Working around NextJS static build limitations, need
+   * useState and useEffect to randomize things on re-render. Without
+   * this everything is statically generated at build time so it never
+   * changes once deployed.
+   * */
+  const [randomNum, setRandomNum] = useState<number>(0.5);
+
+  useEffect(() => {
+    setRandomNum(Math.random());
+  }, []);
+
+  // --- Utilities ---
+  const getRandomInt = (max: number) => Math.floor(randomNum * max);
+
+  const getRandomIntRange = (min: number, max: number) =>
+    Math.floor(randomNum * (max - min) + min);
+
+  /**
+   * Generates negative to positive range, i.e. -15 to 15
+   * getRandomPosNeg(15)
+   */
+  const getRandomIntPosNeg = (range: number) =>
+    Math.floor((randomNum * 2 - 1) * range);
+  // -----------------
+
+  const getRandomLetterStyle = () => ({
+    transform: `
+      translateX(${getRandomIntPosNeg(5)}rem) 
+      translateY(${getRandomIntPosNeg(5)}rem) 
+      rotate(${getRandomInt(360)}deg)`,
+  });
+
+  const getRandomAnimationDuration = () => ({
+    animationDuration: `${getRandomIntRange(40, 60)}s`,
+  });
+
+  const getRandomColorClass = () => {
+    const colorClasses = [styles.color1, styles.color2];
+    return colorClasses[Math.floor(randomNum * colorClasses.length)];
+  };
+
+  return (
+    <div
+      className={`${
+        styles["name-block__letter-container"]
+      } ${getRandomColorClass()}`}>
+      <div
+        className={styles["name-block__letter"]}
+        style={getRandomAnimationDuration()}>
+        <div style={getRandomLetterStyle()}>{letter}</div>
+      </div>
+    </div>
+  );
+};
+
+export const NameTexture = () => {
+  const name = "ADAMROWE";
+
+  return (
+    <div className={styles["name-block"]}>
+      <div className={styles["name-block__filter"]}></div>
+      <div className={styles["name-block__letters"]}>
+        {name.split("").map((letter, i) => (
+          <Letter key={`${letter}-${i}`} letter={letter} />
+        ))}
+      </div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import { NameBlock } from "./components/NameBlock";
+import { NameTexture } from "./components/NameTexture";
 
 export default function Home() {
   return (
-    <main>
-      {/* Adam Rowe, design systems engineer, designer, musician */}
-      <NameBlock />
+    <main className={styles.main}>
+      <div className={styles.info}>
+        <div className={styles.top}>
+          <div className={styles.cloud}>☁︎</div>
+          <div className={styles.name}>Adam Rowe</div>
+        </div>
+        <div className={styles.bottom}>
+          <a href="https://www.linkedin.com/in/admrwe/">
+            Design system engineer
+          </a>
+          , designer, <a href="https://soundcloud.com/admrwe">musician</a>
+        </div>
+      </div>
+      <NameTexture />
     </main>
   );
 }
